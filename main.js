30a06e3ae1e98444327920b483e892145c5120ec
Sebastien Derilus <sebastienderilus@Sebastiens-MacBook-Pro.local>
2022-02-03 18:29:04 -0500

    home work 2

---
+++ main.js
@@ -0,0 +1,25 @@
+// DATA TYPES
+//  //strings
+// 'hello' "world"
+//  //Numbers
+// 123123124 32342.324234
+// BooLean
+// true false
+// //undefined
+// undefined
+// // Null
+// null
+
+
+
+// Variables
+var firstName = 'Sebastien ';
+var lastName='Derilus';
+alert("my name is "+firstName + lastName)
+let age = 21;
+alert("i am "+age+" years old");
+console.log('i am having fun in this class')
+
+
+
+
